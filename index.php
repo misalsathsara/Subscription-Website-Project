65e527ef4a0f73a194030671cef5741c346a73cc
Misal C. Sathsara <info.misalsathsara@gmail.com>
2024-10-05 09:15:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,34 +5,42 @@
 <?php
   include 'hero.php';
 ?>
+<br><br>
 
 <?php
   include 'stat.php';
 ?>
+<br><br>
 
 <?php
   include 'trendingProduct.php';
 ?>
+<br><br>
 
 <?php
   include 'banner.php';
 ?>
+<br><br>
 
 <?php
   include 'productSection.php';
 ?>
+<br><br>
 
 <?php
   include 'testimonials.php';
 ?>
+<br><br>
 
 <?php
   include 'faq.php';
 ?>
+<br><br>
 
 <?php
   include 'bankContainer.php';
 ?>
+<br><br>
 
 <?php
   include 'footer.php';
