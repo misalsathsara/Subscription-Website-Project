781df8151258b8fe8d4f6abb24b05da7edd4978b
Misal C. Sathsara <121123779+misalsathsara@users.noreply.github.com>
2024-12-17 18:58:16 +0530

    Update admin-header.php

--- admin/admin-header.php
+++ admin/admin-header.php
@@ -61,7 +61,7 @@ $activePage = isset($activePage) ? $activePage : '';
 			</a>
 		</li>
 		<li>
-			<a href="#" class="logout">
+			<a href="logout.php" class="logout">
 				<i class='bx bxs-log-out-circle'></i>
 				<span class="text">Logout</span>
 			</a>
